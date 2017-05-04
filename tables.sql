5fa0e1ca41b8ce6957a364c69a46e5b7297eac13
Rui Andrade <ruiandrade@outlook.com>
2017-05-04 17:10:18 +0100

    correcting field types (autoincrement)

--- tables.sql
+++ tables.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Tempo (
-	Tempo_ID int not null check (Tempo_ID > 0),
+	Tempo_ID serial,
 	Hora int not null check (Hora >= 0 and Hora < 24),
 	Dia int not null check (Dia >= 1 and Dia <= 31),
 	Mes int not null check (Mes >= 1 and Mes <= 12),
@@ -7,20 +7,20 @@ CREATE TABLE Tempo (
 );
 
 CREATE TABLE Stand (
-	Stand_ID int not null check (Stand_ID > 0),
-	Nome varchar(255) not null,
+	Stand_ID serial,
+	Nome VARCHAR(255) not null,
 	Lotacao int not null check (Lotacao > 0),
 	PRIMARY KEY (Stand_ID)
 );
 
 CREATE TABLE Taxi (
-	Taxi_ID int not null check (Taxi_ID > 0),
+	Taxi_ID serial,
 	Num_Licenca int not null check (Num_Licenca > 0),
 	PRIMARY KEY (Taxi_ID)
 );
 
 CREATE TABLE Location(
-    Local_ID INT NOT NULL CHECK(Local_ID>0),
+    Local_ID serial,
     Stand_ID INT NOT NULL CHECK(Stand_ID>0),
     Freguesia VARCHAR(50) NOT NULL,
     Concelho VARCHAR(50) NOT NULL,
